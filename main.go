3b93a36854ff351474150c4312ea05d7a74c02ae
Eric Winkelmann <eric@nikelmwann.io>
2016-02-08 22:59:36 -0800

    Add test route which echoes a parameter

--- main.go
+++ main.go
@@ -12,9 +12,14 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintln(w, "HTTP working over a socket!")
 }
 
+func Test(w http.ResponseWriter, r * http.Request, params httprouter.Params) {
+	fmt.Fprintln(w, params.ByName("string"))
+}
+
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
+	router.GET("/test/:string", Test)
 
 	log.Fatal(ListenAndServeUnix("/tmp/dokku-api.sock", os.FileMode(0666), router))
 }
